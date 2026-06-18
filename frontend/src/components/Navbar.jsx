25ec739a18a09354e77806c28a20db6cc48905a1
JaganParida <jagan.parida.dev@gmail.com>
2026-06-18 14:13:07 +0530

    Add Become a host to Navbar dropdown menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -139,6 +139,7 @@ const Navbar = () => {
                     <Link to="/signup" onClick={() => setIsDropdownOpen(false)} className="block px-4 py-3 font-bold text-gray-900 hover:bg-gray-100 transition">Sign up</Link>
                     <Link to="/login" onClick={() => setIsDropdownOpen(false)} className="block px-4 py-3 text-gray-700 hover:bg-gray-100 transition">Log in</Link>
                     <hr className="my-1 border-gray-200" />
+                    <Link to="/host/signup" onClick={() => setIsDropdownOpen(false)} className="block px-4 py-3 text-gray-700 hover:bg-gray-100 transition">Become a host</Link>
                     <Link to="/" onClick={() => setIsDropdownOpen(false)} className="block px-4 py-3 text-gray-700 hover:bg-gray-100 transition">Help Center</Link>
                   </>
                 )}
